a9108d1221319d4086a558e39a255786d8f15f0d
Your Name <you@example.com>
2024-01-07 11:14:45 +0600

    Modify Routers import method.

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "../pages/Shared/Header/Header";
+import Footer from "../pages/Shared/Footer/Footer";
 
 const Main = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </>
     );
 };
 
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,12 +1,12 @@
-const { createBrowserRouter } = require("react-router-dom");
-const { default: Main } = require("../Layout/Main");
-const { default: NotFound } = require("../pages/NotFound/NotFound");
-const { default: Home } = require("../pages/Home/Home/Home");
-const { default: AddCoffee } = require("../pages/AddCoffee/AddCoffee");
-const { default: ViewCoffee } = require("../pages/ViewCoffee/ViewCoffee");
-const { default: EditCoffee } = require("../pages/EditCoffee/EditCoffee");
-const { default: Login } = require("../pages/Login/Login");
-const { default: Register } = require("../pages/Register/Register");
+import { createBrowserRouter } from "react-router-dom";
+import Main from '../Layout/Main';
+import NotFound from '../pages/NotFound/NotFound';
+import Home from '../pages/Home/Home/Home';
+import AddCoffee from '../pages/AddCoffee/AddCoffee';
+import ViewCoffee from '../pages/ViewCoffee/ViewCoffee';
+import EditCoffee from '../pages/EditCoffee/EditCoffee';
+import Login from '../pages/Login/Login';
+import Register from '../pages/Register/Register';
 
 const router = createBrowserRouter([
     {
