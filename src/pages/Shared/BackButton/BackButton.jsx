ae0aa9cb2605cfdce0960fe6269fe14018d41fa7
Your Name <you@example.com>
2024-01-07 19:00:32 +0600

    Add hover effect on Back To home button

--- src/pages/Shared/BackButton/BackButton.jsx
+++ src/pages/Shared/BackButton/BackButton.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const BackButton = () => {
     return (
         <div className="mb-11">
-            <Link to='/' className="text-gray-700 text-3xl flex items-center gap-4 font-normal font-['Rancho']">
+            <Link to='/' className="text-gray-700 w-48 rounded-lg duration-300 hover:text-white hover:bg-[#D2B48C] text-3xl flex items-center gap-4 font-normal font-['Rancho']">
                 <IoIosReturnLeft />
                 <span>Back to home</span>
             </Link>
