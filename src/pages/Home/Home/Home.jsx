4a6de3d2f8b3653e8b7241ddf44111b74f30bb28
Your Name <you@example.com>
2024-01-07 13:09:13 +0600

    Implement Hero Section in Home Page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,16 @@
-
+import { Link } from 'react-router-dom';
+import HeroBg from '../../../assets/images/more/3.png'
 const Home = () => {
     return (
-        <div>
-            
+        <div style={{backgroundImage: `url(${HeroBg})`}} className='bg-cover bg-center'>
+            <div className="container mx-auto px-8">
+                <div className='py-16 lg:py-48 flex lg:w-1/2 mx-auto justify-center flex-col text-center items-center'>
+                <h1 className="text-white text-5xl lg:text-[55px] font-normal font-['Rancho']">Would you like a Cup of Delicious Coffee?</h1>
+                <p className="text-white text-base font-normal mt-4 mb-8 font-['Raleway'] leading-[30px]">Its coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your companion of every moment!!! Enjoy the beautiful moments and make them memorable.</p>
+                
+                <Link to='/addCoffee' className="px-4 py-1 bg-orange-300 text-neutral-800 text-2xl font-normal font-['Rancho']">Add Coffee</Link>
+                </div>
+            </div>
         </div>
     );
 };
