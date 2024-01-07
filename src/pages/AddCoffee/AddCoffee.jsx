828109dacb4d139ef08402a245a1d2bbf5af667f
Your Name <you@example.com>
2024-01-07 18:33:04 +0600

    Implement 404 page

--- src/pages/AddCoffee/AddCoffee.jsx
+++ src/pages/AddCoffee/AddCoffee.jsx
@@ -12,45 +12,45 @@ const AddCoffee = () => {
                         <span>Back to home</span>
                     </Link>
                 </div>
-                <div className="bg-stone-100 rounded-[5px] py-10 px-12 lg:py-14 lg:px-20">
+                <div className="bg-stone-100 rounded-[5px] py-10 px-12 lg:py-12 lg:pb-14 lg:px-20">
                     <div className="text-center lg:w-3/4 mx-auto">
-                        <h2 className="text-gray-700 text-[45px] font-normal font-['Rancho']">Add New Coffee</h2>
+                        <h2 className="text-gray-700 text-[45px] mb-2 font-normal font-['Rancho']">Add New Coffee</h2>
                         <p className="text-center text-stone-900 text-opacity-70 text-lg font-normal font-['Raleway'] leading-[30px]">It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
                     </div>
                     <form className="grid lg:grid-cols-2 mt-8 gap-8">
                         <div className="w-full">
                             <label className="text-stone-900 text-opacity-80 text-xl font-semibold font-['Raleway'] leading-[30px]">Name</label>
-                            <input type="text" name="name" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]" placeholder="Enter coffee Name" />
+                            <input type="text" name="name" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]"  placeholder="Enter coffee Name" required />
                         </div>
                         <div className="w-full">
                             <label className="text-stone-900 text-opacity-80 text-xl font-semibold font-['Raleway'] leading-[30px]">Price</label>
-                            <input type="text" name="price" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]" placeholder="Enter coffee Price" />
+                            <input type="text" name="price" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]"  placeholder="Enter coffee Price" required />
                         </div>
                         <div className="w-full">
                             <label className="text-stone-900 text-opacity-80 text-xl font-semibold font-['Raleway'] leading-[30px]">Chef</label>
-                            <input type="text" name="chef" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]" placeholder="Enter coffee chef" />
+                            <input type="text" name="chef" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]"  placeholder="Enter coffee chef" required />
                         </div>
                         <div className="w-full">
                             <label className="text-stone-900 text-opacity-80 text-xl font-semibold font-['Raleway'] leading-[30px]">Supplier</label>
-                            <input type="text" name="supplier" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]" placeholder="Enter coffee supplier" />
+                            <input type="text" name="supplier" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]"  placeholder="Enter coffee supplier" required />
                         </div>
                         <div className="w-full">
                             <label className="text-stone-900 text-opacity-80 text-xl font-semibold font-['Raleway'] leading-[30px]">Taste</label>
-                            <input type="text" name="taste" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]" placeholder="Enter coffee taste" />
+                            <input type="text" name="taste" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]"  placeholder="Enter coffee taste" required />
                         </div>
                         <div className="w-full">
                             <label className="text-stone-900 text-opacity-80 text-xl font-semibold font-['Raleway'] leading-[30px]">Category</label>
-                            <input type="text" name="category" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]" placeholder="Enter coffee category" />
+                            <input type="text" name="category" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]"  placeholder="Enter coffee category" required />
                         </div>
                         <div className="w-full">
                             <label className="text-stone-900 text-opacity-80 text-xl font-semibold font-['Raleway'] leading-[30px]">Details</label>
-                            <input type="text" name="details" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]" placeholder="Enter coffee details" />
+                            <input type="text" name="details" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]"  placeholder="Enter coffee details" required />
                         </div>
                         <div className="w-full">
                             <label className="text-stone-900 text-opacity-80 text-xl font-semibold font-['Raleway'] leading-[30px]">Photo</label>
-                            <input type="text" name="photo" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]" placeholder="Enter photo URL" />
+                            <input type="text" name="photo" className="w-full mt-3 border-none focus:border bg-white rounded-[5px]"  placeholder="Enter photo URL" required />
                         </div>
-                        <input type="submit" className="bg-[#D2B48C] py-1 mt-3 lg:col-span-2 rounded-[5px] border-2 border-stone-800 text-center text-stone-800 text-2xl font-normal font-['Rancho']" value="Add Coffee" />
+                        <input type="submit" className="bg-[#D2B48C] py-1 mt-3 lg:col-span-2 rounded-[5px] border-2 border-stone-800 text-center cursor-pointer hover:bg-transparent duration-300 text-stone-800 text-2xl font-normal font-['Rancho']" value="Add Coffee" />
                     </form>
                 </div>
             </div>
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom";
+import Header from "../Shared/Header/Header";
+import { IoIosReturnLeft } from "react-icons/io";
+import NotFoundImg from '../../assets/images/404/404.gif'
 
 const NotFound = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Header></Header>
+            <div className="container mx-auto px-8">
+                <div className="flex justify-center pt-8 lg:pt-16 items-center flex-col">
+                    <div className="mb-11">
+                        <Link to='/' className="text-gray-700 text-3xl flex items-center gap-4 font-normal font-['Rancho']">
+                            <IoIosReturnLeft />
+                            <span>Back to home</span>
+                        </Link>
+
+                    </div>
+                    <div className="">
+                        <Link>
+                            <img src={NotFoundImg} alt="" />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
